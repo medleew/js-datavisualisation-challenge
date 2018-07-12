83bde0f8fb10afb9b34acc7250d7183a163850b8
medleew <nadir.medlee@gmail.com>
2018-07-12 15:36:56 +0200

    3

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,27 @@
-// function that generate random color in rgba for the every line in the chart
 function getRandomRgb() {
   let o = Math.round, r = Math.random, s = 255;
   return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
 }
 
 
-addEventListener("load", addFirstInlineDataGraph);
+addEventListener("load", addInlineDataGraph);
 
-function addFirstInlineDataGraph() {
-  let table = document.getElementById('table1');
-  let parentTab = table.parentNode;
+function addInlineDataGraph() {
+  const table = document.getElementById('table1');
+  const parentTab = table.parentNode;
 
   let divGraph = document.createElement('div');
-  divGraph.id = "FirstChartDiv";
+  divGraph.id = "datavis-div1";
   parentTab.insertBefore(divGraph, table);
 
-  let svg = dimple.newSvg("#"+divGraph.id, 590, 750);
-  let data = getDataFromTable(table); // à crée ---> 
+  let svg = dimple.newSvg("#"+divGraph.id, 900, 1000);
+  let data = getDataFromTable(table); 
   let myChart = new dimple.chart(svg, data);
-  myChart.setBounds(60, 30, 505, 705);
+  myChart.setBounds(60, 30, 650, 705);
   let x = myChart.addCategoryAxis("x", "Year");
-  let y = myChart.addMeasureAxis("y", "Numbers");
-  // myChart.addLegend(10, 10, 100% 20, "right");
+  let y = myChart.addMeasureAxis("y", "Nombre");
+  myChart.addLegend(10, 1, '100%', 200, "right");
   myChart.addSeries("Country", dimple.plot.line, [x, y]);
-  myChart.ease = "bounce";
   myChart.defaultColors = [
     new dimple.color(getRandomRgb()),
     new dimple.color(getRandomRgb()),
@@ -58,57 +56,56 @@ function addFirstInlineDataGraph() {
     new dimple.color(getRandomRgb()),
     new dimple.color(getRandomRgb()),
   ]; 
+  myChart.ease = "bounce";
   myChart.draw();
 }
 
 
-// Requires a DOM element
 function getDataFromTable(table) {
-  let rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
-  let rowsNbre = rows.length;
-  let dataObj = [];
-
-  // Stores years for Y data
-  let years = [];
-
-  for (let i = 0; i < rowsNbre; i++) {
-
-    // Skip if it's the first row (titles)
-    if(i === 0) {
-      let cells = rows[0].getElementsByTagName('th');
-      let celluleNbre = cells.length;
-
-      for (let j = 0; j < celluleNbre; j++) {
-        // Skip first two matches (they're empty)
-        if (j < 1) continue;
-        // Push the data for the rest
-        years.push(cells[j].innerHTML);
+    let rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    const rowsNbre = rows.length;
+    let dataObj = [];
+  
+    // Stores years for Y data
+    let years = [];
+  
+    for (let i = 0; i < rowsNbre; i++) {
+  
+      // Skip if it's the first row (titles)
+      if(i === 0) {
+        let cells = rows[0].getElementsByTagName('th');
+        let celluleNbre = cells.length;
+  
+        for (let j = 0; j < celluleNbre; j++) {
+          // Skip first two matches (they're empty)
+          if (j < 2) continue;
+          // Push the data for the rest
+          years.push(cells[j].innerHTML);
+        }
+        continue;
       }
-      continue;
+  
+      // Otherwise loop through the data
+      let cells = rows[i].getElementsByTagName('td');
+      let cellsLength = cells.length;
+  
+      let rowCountry = cells[0].innerHTML;
+  
+      for (let j = 1; j < cellsLength; j++) {
+          let dataPoint = {"Country":rowCountry};
+          dataPoint["Year"] = years[j-1];
+          let nb = parseFloat(cells[j].innerHTML.replace(",", "."))*1000;
+          dataPoint["Nombre"] = Number.isNaN(nb) ? 0 : nb;
+          dataObj.push(dataPoint);
+      }
+      // End cells loop
+  
     }
-
-    // Otherwise loop through the data
-    let cells = rows[i].getElementsByTagName('td');
-    let cellsLength = cells.length;
-
-    let rowCountry = cells[0].innerHTML;
-
-    for (let j = 1; j < cellsLength; j++) {
-        let dataPoint = {"Country":rowCountry};
-        dataPoint["Year"] = years[j-1];
-        let nb = parseFloat(cells[j].innerHTML.replace(",", "."))*1000;
-        dataPoint["Nombre"] = Number.isNaN(nb) ? 0 : nb;
-
-        dataObj.push(dataPoint);
-    }
-    // End cells loop
-
+    // End row loop
+  
+    return dataObj;
+  
   }
-  // End row loop
-
-  return dataObj;
-
-}
 
 // remote data in real time via ajax:
 
@@ -153,134 +150,134 @@ function getDataFromTable(table) {
 
 // second chart 
 
-//   addEventListener("load", addSecondInlineDataGraph);
+  addEventListener("load", addSecondInlineDataGraph);
 
-// function addSecondInlineDataGraph() {
-//   let table = document.getElementById('table2');
+function addSecondInlineDataGraph() {
+  let table = document.getElementById('table2');
+  let parentTab = table.parentNode;
+
+  let divGraph2 = document.createElement('div');
+  divGraph2.id = "SecondChartDiv";
+
+  parentTab.insertBefore(divGraph2, table);
+  // // Get data from an external location and generate the chart when it returns
+  // d3.json("MyAwesomeDataSource.json", function (data) {
+  // 	// Add an svg object to the body element
+  // 	var svg = dimple.newSvg("body", 1200, 800);
+  // 	// Create a new chart object based on this data and svg
+  // 	var myChart = new dimple.chart(svg, data);
+  // 	...
+  // });
+  // // Check the number of axes added before and after adding
+  // console.log(myChart.axes.length); // Logs 0
+  // myChart.addMeasureAxis("x", "Sales Volume");
+  // console.log(myChart.axes.length); // Logs 1
+  let svg = dimple.newSvg("#"+divGraph2.id, 590, 800);
+  let data = getDataFromTable2(table); 
+  let myChart = new dimple.chart(svg, data);
+  myChart.setBounds(60, 30, 505, 705);
+  let x = myChart.addCategoryAxis("x", "Year");
+  let y = myChart.addMeasureAxis("y", "Pc");
+  // myChart.addLegend(60, 10, 500, 20, "right");
+  myChart.addSeries("Country", dimple.plot.line, [x, y]);
+  myChart.defaultColors = [
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb()),
+    new dimple.color(getRandomRgb())
+  ]; 
+  myChart.draw();
+}
+
+
+// Requires a DOM element
+function getDataFromTable2(table) {
+    let rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    let rowsNbre = rows.length;
+    let dataObj2 = [];
+  
+    // Stores years for Y data
+    let years = [];
+  
+    for (let i = 0; i < rowsNbre; i++) {
+  
+      // Skip if it's the first row (titles)
+      if(i === 0) {
+        let cells = rows[0].getElementsByTagName('th');
+        let celluleNbre = cells.length;
+  
+        for (let j = 0; j < celluleNbre; j++) {
+          // Skip first two matches (they're empty)
+          if (j < 1) continue;
+          // Push the data for the rest
+          years.push(cells[j].innerHTML);
+        }
+        continue;
+      }
+  
+      // Otherwise loop through the data
+      let cells = rows[i].getElementsByTagName('td');
+      let cellsLength = cells.length;
+  
+      let rowCountry = cells[0].innerHTML;
+  
+      for (let j = 1; j < cellsLength; j++) {
+          let dataPoint = {"Country":rowCountry};
+          dataPoint["Year"] = years[j-1];
+          let nb = parseFloat(cells[j].innerHTML.replace(",", "."))*1000;
+          dataPoint["Nombre"] = Number.isNaN(nb) ? 0 : nb;
+  
+          dataObj2.push(dataPoint);
+      }
+      // End cells loop
+  
+    }
+    // End row loop
+  
+    return dataObj2;
+  
+  }
+
+//   remote data in real time via ajax:
+
+//   let table = document.getElementById('firstHeading');
 //   let parentTab = table.parentNode;
 
-//   let divGraph = document.createElement('div');
-//   divGraph.id = "SecondChartDiv";
-
-//   parentTab.insertBefore(divGraph, table);
-//   // // Get data from an external location and generate the chart when it returns
-//   // d3.json("MyAwesomeDataSource.json", function (data) {
-//   // 	// Add an svg object to the body element
-//   // 	var svg = dimple.newSvg("body", 1200, 800);
-//   // 	// Create a new chart object based on this data and svg
-//   // 	var myChart = new dimple.chart(svg, data);
-//   // 	...
-//   // });
-//   // // Check the number of axes added before and after adding
-//   // console.log(myChart.axes.length); // Logs 0
-//   // myChart.addMeasureAxis("x", "Sales Volume");
-//   // console.log(myChart.axes.length); // Logs 1
-//   let svg = dimple.newSvg("#"+divGraph.id, 590, 800);
-//   let data = getDataFromTable(table); 
-//   let myChart = new dimple.chart(svg, data);
-//   myChart.setBounds(60, 30, 505, 705);
-//   let x = myChart.addCategoryAxis("x", "Year");
-//   let y = myChart.addMeasureAxis("y", "Nombre");
-//   // myChart.addLegend(60, 10, 500, 20, "right");
-//   myChart.addSeries("Country", dimple.plot.line, [x, y]);
-//   myChart.defaultColors = [
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb()),
-//     new dimple.color(getRandomRgb())
-//   ]; 
-//   myChart.draw();
-// }
-
-
-// // Requires a DOM element
-// function getDataFromTable(table) {
-//     let rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
-//     let rowsNbre = rows.length;
-//     let dataObj = [];
-  
-//     // Stores years for Y data
-//     let years = [];
-  
-//     for (let i = 0; i < rowsNbre; i++) {
-  
-//       // Skip if it's the first row (titles)
-//       if(i === 0) {
-//         let cells = rows[0].getElementsByTagName('th');
-//         let celluleNbre = cells.length;
-  
-//         for (let j = 0; j < celluleNbre; j++) {
-//           // Skip first two matches (they're empty)
-//           if (j < 1) continue;
-//           // Push the data for the rest
-//           years.push(cells[j].innerHTML);
-//         }
-//         continue;
-//       }
-  
-//       // Otherwise loop through the data
-//       let cells = rows[i].getElementsByTagName('td');
-//       let cellsLength = cells.length;
-  
-//       let rowCountry = cells[0].innerHTML;
-  
-//       for (let j = 1; j < cellsLength; j++) {
-//           let dataPoint = {"Country":rowCountry};
-//           dataPoint["Year"] = years[j-1];
-//           let nb = parseFloat(cells[j].innerHTML.replace(",", "."))*1000;
-//           dataPoint["Nombre"] = Number.isNaN(nb) ? 0 : nb;
-  
-//           dataObj.push(dataPoint);
-//       }
-//       // End cells loop
-  
-//     }
-//     // End row loop
-  
-//     return dataObj;
-  
-//   }
-
-  // remote data in real time via ajax:
-
-  // let table = document.getElementById('firstHeading');
-  // let parentTab = table.parentNode;
-
-  // let divGraphInRealTime = document.createElement('div');
-  // divGraph.id = "divGraphInRealTime";
+//   let divGraphInRealTime = document.createElement('div');
+//   divGraph.id = "divGraphInRealTime";
 
 
 // let data = [];
 
-// let table = document.getElementById('table1');
+// let table = document.getElementById('table2');
 
 // let years = table.getElementsByTagName('tr')[1].getElementsByTagName('th');
 // let yearsArray = [];
@@ -307,3 +304,9 @@ function getDataFromTable(table) {
 // }
 
 // console.log(data);
+
+
+
+
+
+
